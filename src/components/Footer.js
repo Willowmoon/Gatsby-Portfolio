906dbd4c94205ba436fe6f7c1832e3196b733961
willowmoon <willowmoon@pop-os.localdomain>
2020-05-19 19:10:05 -0400

    clear footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,8 @@
 import React from "react"
-import socialIcons from "../constants/social-icons"
 
 const Footer = () => {
   return (
     <footer>
-      <div>
-        {socialIcons.map((item, index) => {
-          return (
-            <a
-              key={index}
-              href={item.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              &nbsp;{item.icon}
-              &nbsp;
-            </a>
-          )
-        })}
-      </div>
       <div style={{ fontSize: "10px" }}>
         Copyright &copy; Douglas Kelley {new Date().getFullYear()}&nbsp; //
         Background Photo by
