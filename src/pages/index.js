210930c5d80869751811d744fe46379d94c1ab8e
willowmoon <willowmoon@pop-os.localdomain>
2020-07-01 16:17:08 -0400

    clean and image optimization

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,27 +4,29 @@ import logo from "../images/logo.png"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 const IndexPage = () => (
-  <div>
-    <SEO title="Home" />
-    <main>
-      <img className="first" src={logo} width="35%" alt="Douglas Kelley" />
-      <h1>douglas kelley</h1>
+  
+    <div>
+      <SEO title="Home" />
+      <main>
+        <img className="first" src={logo} width="35%" alt="Douglas Kelley" />
+        <h1>douglas kelley</h1>
 
-      <div className="center">
-        <p>- web developer -</p>
-        <br />
-        <AniLink fade to="/about">
-          <button className="btn">about</button>
-        </AniLink>
-        <AniLink fade to="/projects">
-          <button className="btn">projects</button>
-        </AniLink>
-        <AniLink fade to="/contact">
-          <button className="btn">contact</button>
-        </AniLink>
-      </div>
-    </main>
-  </div>
+        <div className="center">
+          <p>- web developer -</p>
+          <br />
+          <AniLink fade to="/about">
+            <button className="btn">about</button>
+          </AniLink>
+          <AniLink fade to="/projects">
+            <button className="btn">projects</button>
+          </AniLink>
+          <AniLink fade to="/contact">
+            <button className="btn">contact</button>
+          </AniLink>
+        </div>
+      </main>
+    </div>
 )
 
 export default IndexPage
+
